5e3937eb2bca11d47681d94437668675c2f2f875
Franzie Yoogan <franzieyoogan@gmail.com>
2024-05-20 20:05:22 -0300

    submit

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HttpClientModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  private http = inject(HttpClient)
+  
+  submit() {
+
+    const userEmail:any = document.getElementById('userEmail')
+    const userName:any = document.getElementById('userName')
+    const userPassword:any = document.getElementById('userPassword')
+    const containerAlert:any = document.getElementById('containerAlert')
+
+    const body = {
+
+      "userName": userName.value,
+      "userEmail": userEmail.value,
+      "userPassword": userPassword.value
+
+    }
+
+
+    this.http.post('http://localhost:3000/trainer', body).subscribe(response => {
+      console.log('Registered:', response);
+
+    });
+
+
+
+   
+
+  
+  
+
+}
 
 }
